e5f971c6ac0b38c000140352a7941af07e395f42
Jaril <jarilvalenciano@gmail.com>
2022-07-25 17:02:27 -0700

    Add playwright@v1.19 and run mock test locally

---
+++ playwright.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+  },
+};
+
+module.exports = config;
